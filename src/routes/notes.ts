aa52dab8b5a1658a9101cf935d81837c7a0cc3b0
Necralma <lihuel.almada@gmail.com>
2020-12-15 15:05:56 -0300

    Authentication notes

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,11 +1,36 @@
 import {Router} from 'express';
 const router = Router();
 import * as noteController from '../controller/notes';
+import passport from "passport";
 //define URL
-router.post('/notes', noteController.createNote);
-router.get('/notes', noteController.getNotes);
-router.get('/notes/:id', noteController.getNote);
-router.delete('/notes/:id', noteController.deleteNote);
-router.put('/notes/:id', noteController.updateNotes);
-
+router.post(
+    "/notes",
+    //the Strategy in Auth mddleware is "jwt" for default
+    passport.authenticate("jwt", { session: false }),
+    noteController.createNote
+);
+router.get(
+    "/notes",
+    //the Strategy in Auth mddleware is "jwt" for default
+    passport.authenticate("jwt", { session: false }),
+    noteController.getNotes
+);  
+router.get(
+    "/notes/:id",
+    //the Strategy in Auth mddleware is "jwt" for default
+    passport.authenticate("jwt", { session: false }),
+    noteController.getNote
+); 
+router.delete(
+    "/notes/:id",
+    //the Strategy in Auth mddleware is "jwt" for default
+    passport.authenticate("jwt", { session: false }),
+    noteController.deleteNote
+); 
+router.put(
+    "/notes/:id",
+    //the Strategy in Auth mddleware is "jwt" for default
+    passport.authenticate("jwt", { session: false }),
+    noteController.updateNotes
+); 
 export default router
